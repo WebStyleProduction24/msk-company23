3e95b2bf1a6c9e21c8187bc9451648ef36984578
Jack36VRN <Jack36VRN@users.noreply.github.com>
2018-09-29 02:17:15 +0300

    retest

--- wp-content/themes/philips/functions.php
+++ wp-content/themes/philips/functions.php
@@ -465,79 +465,79 @@ if ( ! function_exists( 'philips_setup' ) ) :
  */
 function philips_setup() {
 
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on philips, use a find and replace
-	 * to change 'philips' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'philips', get_template_directory() . '/languages' );
+    /*
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on philips, use a find and replace
+     * to change 'philips' to the name of your theme in all the template files
+     */
+    load_theme_textdomain( 'philips', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
 
-	// Управление заголовком из Wordpress.
+    // Управление заголовком из Wordpress.
     add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+     */
+    //add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary Menu', 'philips' ),
-	) );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'primary' => esc_html__( 'Primary Menu', 'philips' ),
+    ) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
-	) );
-	
-	/*
-	 * Enable support for Post Thumbnails.
-	 * See https://codex.wordpress.org/Function_Reference/add_theme_support
-	 */
-	
-	add_theme_support( 'post-thumbnails' );	
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support( 'html5', array(
+        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+    ) );
+    
+    /*
+     * Enable support for Post Thumbnails.
+     * See https://codex.wordpress.org/Function_Reference/add_theme_support
+     */
+    
+    add_theme_support( 'post-thumbnails' ); 
     add_image_size('philips-blog-thumbnails', 960, 450, true);
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link','gallery',
-	) );
+    /*
+     * Enable support for Post Formats.
+     * See http://codex.wordpress.org/Post_Formats
+     */
+    add_theme_support( 'post-formats', array(
+        'aside', 'image', 'video', 'quote', 'link','gallery',
+    ) );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'philips_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+    // Set up the WordPress core custom background feature.
+    add_theme_support( 'custom-background', apply_filters( 'philips_custom_background_args', array(
+        'default-color' => 'ffffff',
+        'default-image' => '',
+    ) ) );
 }
 endif; // philips_setup
 add_action( 'after_setup_theme', 'philips_setup' );
 
 function philips_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'philips_content_width', 640 );
+    $GLOBALS['content_width'] = apply_filters( 'philips_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'philips_content_width', 0 );
 
 function philips_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'philips' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'philips' ),
+        'id'            => 'sidebar-1',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ) );
 }
 add_action( 'widgets_init', 'philips_widgets_init' );
 
@@ -545,17 +545,17 @@ add_action( 'widgets_init', 'philips_widgets_init' );
  * Enqueue scripts and styles.
  */
 function philips_scripts() {
-	wp_enqueue_style( 'philips-bootstrap', get_template_directory_uri() . '/css/bootstrap.css' );
-	wp_enqueue_style( 'philips-fontawesome', get_template_directory_uri() . '/css/font-awesome.css' );
-	wp_enqueue_style( 'philips-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'philips-responsive', get_template_directory_uri() . '/css/responsive.css' );
+    wp_enqueue_style( 'philips-bootstrap', get_template_directory_uri() . '/css/bootstrap.css' );
+    wp_enqueue_style( 'philips-fontawesome', get_template_directory_uri() . '/css/font-awesome.css' );
+    wp_enqueue_style( 'philips-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'philips-responsive', get_template_directory_uri() . '/css/responsive.css' );
 
-	wp_enqueue_script( 'philips-bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), '20120206', true );
+    wp_enqueue_script( 'philips-bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), '20120206', true );
 
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'philips_scripts' );
 
