b9a472e865bc9c35bb0fe6a13f0b674add1e15bc
Jack36VRN <jack3@JACK36-LAPTOP>
2018-12-12 14:58:26 +0300

    test

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'msk-company23.ru');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'еуые');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
