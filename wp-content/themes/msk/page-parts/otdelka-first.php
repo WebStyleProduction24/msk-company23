141a44059ab1960d5ef1af080499c47744be501f
Jack36VRN <jack3@JACK36-LAPTOP>
2018-12-25 13:34:01 +0300

    Update 2018/12/25 13:33
    Готов первый блок на странице "Отделка и ремонт под ключ" для всех типов экранов

--- wp-content/themes/msk/page-parts/otdelka-first.php
+++ wp-content/themes/msk/page-parts/otdelka-first.php
@@ -6,21 +6,33 @@
  */
 ?>
 
-
 <section id="first" style="background: none;padding-top: 0px;BOTTOM: 0;margin-top: 0;">
-<div class="row" style="margin-top: 0em;">
-<div class="large-12 columns">
-        <div class="twentytwenty-container"><img src="/wp-content/themes/msk/1_2.jpg" />
-          <img src="/wp-content/themes/msk/1_1.jpg" />
-          
-        </div>
-      </div>
-    </div>
-  <header class="entry-header col-md-6 main l2"><img class="fgf" src="/wp-content/uploads/2016/12/krisha.png"/>
-	<h1 class="entry-title">РЕМОНТ И ОТДЕЛКА <br>КОТТЕДЖЕЙ В СОЧИ <br>ПОД КЛЮЧ</h1><h3 class="w19">КАЧЕСТВО, СОБЛЮДЕНИЕ СРОКОВ, ЛЮБАЯ СЛОЖНОСТЬ</h3><div class="posab"><a href="#contact_form_pop" class="fancybox">• ЗАКАЗАТЬ КОНСУЛЬТАЦИЮ •</a></div>
+	<div class="row" style="margin-top: 0em;">
+		<div class="large-12 columns">
+			<div class="twentytwenty-container"><img src="<?php echo get_template_directory_uri(); ?>/1_2.jpg" />
+				<img src="<?php echo get_template_directory_uri(); ?>/1_1.jpg" />
+
+			</div>
+		</div>
+	</div>
+	
+
+
+
+	<header class="entry-header col-md-4 main">
+		<img class="fgf" src="<?php echo get_template_directory_uri(); ?>/images/msk.png"/>
+		<h1 class="entry-title">Ремонт и отделка коттеджей в&nbsp;Сочи под&nbsp;ключ</h1>
+		<h3 class="w19">Качество, соблюдение сроков, любая сложность</h3>		
+
+		<div class="posab"><a href="#contact_form_pop" class="fancybox brown two wpcf7-form-control wpcf7-submit">• ЗАКАЗАТЬ КОНСУЛЬТАЦИЮ •</a></div>
 	</header>
-</section>	
 
+</section>
 
-<script>$(window).load(function(){ $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.3});$(".twentytwenty-container[data-orientation='vertical']").twentytwenty({default_offset_pct: 0.3, orientation: 'vertical'});});</script>
+<script>
+	$(window).load(function(){
+		$(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.3});
+		$(".twentytwenty-container[data-orientation='vertical']").twentytwenty({default_offset_pct: 0.3, orientation: 'vertical'});
+	});
+</script>
 
