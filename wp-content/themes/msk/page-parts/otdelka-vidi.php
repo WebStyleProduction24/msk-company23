d375d50a8ac6cea738100580c4db29c049b0f4cc
Jack36VRN <jack3@JACK36-LAPTOP>
2018-12-28 16:29:13 +0300

    Update 2018/12/28 16:29
    Готов блок "Виды ремонта" для страницы "Отделка и ремонт под ключ" для всех устройств

--- wp-content/themes/msk/page-parts/otdelka-vidi.php
+++ wp-content/themes/msk/page-parts/otdelka-vidi.php
@@ -11,20 +11,20 @@
 		<h3 class="l2-zag">• ВИДЫ РЕМОНТА •</h3>  
 		<div class="col-md-12">
 			<div class="col-md-4">
-				<img src="/wp-content/uploads/2016/12/y1-e1482770348751.png"/>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/otdelka/1.png"/>
 				<h3 class="l2-zag1">
 					<span class="hidden-xs hidden-sm">&bull; </span>Косметический<span class="hidden-xs hidden-sm"> &bull;</span>
 				</h3>
 				<a href="#h1" class="fancybox pod">Подробности</a>
 			</div>
 			<div class="col-md-4">
-				<img src="/wp-content/uploads/2016/12/y2.png"/>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/otdelka/2.png"/>
 				<h3 class="l2-zag1">
 					<span class="hidden-xs hidden-sm">&bull; </span>Капитальный<span class="hidden-xs hidden-sm"> &bull;</span>
 				</h3>
 				<a href="#h2" class="fancybox pod">Подробности</a>
 			</div><div class="col-md-4">
-				<img src="/wp-content/uploads/2016/12/y3.png"/>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/otdelka/3.png"/>
 				<h3 class="l2-zag1">
 					<span class="hidden-xs hidden-sm">&bull; </span>Евроремонт<span class="hidden-xs hidden-sm"> &bull;</span>
 				</h3>
